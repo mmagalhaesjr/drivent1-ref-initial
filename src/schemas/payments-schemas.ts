48f515350953535fc04dad959e1440251838ee28
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-04-18 17:39:15 -0300

    update

--- src/schemas/payments-schemas.ts
+++ src/schemas/payments-schemas.ts
@@ -12,11 +12,5 @@ export type PaymentSchema = {
 };
 export const paymentSchema: ObjectSchema<PaymentSchema> = Joi.object({
   ticketId: Joi.number().required(),
-  cardData: Joi.object({
-    issuer: Joi.string().required(),
-    number: Joi.number().required(),
-    name: Joi.string().required(),
-    expirationDate: Joi.date().required(),
-    cvv: Joi.number().required(),
-  }).required(),
+  cardData: Joi.object().required(),
 });
