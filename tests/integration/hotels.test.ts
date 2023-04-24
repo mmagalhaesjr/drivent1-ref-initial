9a157c3d998a05ae361be1ec8770a604800eabd7
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-04-24 17:35:59 -0300

    update:GET/hotls integration tests

--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -9,6 +9,7 @@ import {
   createTicket,
   createTicketType,
   createTicketTypeRemote,
+  createTicketTypeWitHotels,
   createTicketTypeWithHotel,
   createUser,
 } from '../factories';
@@ -105,6 +106,84 @@ describe('GET /hotels', () => {
 
       const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
       expect(response.status).toEqual(httpStatus.PAYMENT_REQUIRED);
+    });
+    it('should return 404 if there is no hotel', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWitHotels();
+
+      await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
+    });
+
+    it('should respond with 200 and hotels information', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWitHotels();
+
+      await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const hotel = await createHotel();
+      const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(httpStatus.OK);
+      expect(response.body).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(Number),
+            name: hotel.name,
+            image: hotel.image,
+            createdAt: hotel.createdAt.toISOString(),
+            updatedAt: hotel.updatedAt.toISOString(),
+          }),
+        ]),
+      );
+    });
+
+    describe('GET /hotels/:hotelId', () => {
+      it('Should respond 401 if token is not given', async () => {
+        const response = await server.get(`/hotels/${1}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+      });
+
+      it('Should respond 401 if token is not valid', async () => {
+        const token = faker.lorem.word;
+
+        const response = await server.get(`/hotels/${1}`).set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+      });
+
+      it('Should respond with 401 if there is no session for given token', async () => {
+        const userWithoutSession = await createUser();
+        const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+        const response = await server.get(`/hotels/${1}`).set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+      });
+
+      describe('when token is valid', () => {
+        it('Should respond with 400 if hotelId is invalid', async () => {
+          const user = await createUser();
+          const token = await generateValidToken(user);
+          const enrollment = await createEnrollmentWithAddress(user);
+          const ticketType = await createTicketTypeWitHotels();
+
+          await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+          const response = await server.get('/hotels/noId').set('Authorization', `Bearer ${token}`);
+          expect(response.status).toBe(httpStatus.BAD_REQUEST);
+        });
+        it('should respond with 404 if there is no enrollment', async () => {
+          const user = await createUser();
+          const token = await generateValidToken(user);
+          const hotel = await createHotel();
+
+          const response = await server.get(`/hotels/${hotel.id}`).set('Authorization', `Bearer ${token}`);
+          expect(response.status).toBe(httpStatus.NOT_FOUND);
+        });
+      });
     });
   });
 });
